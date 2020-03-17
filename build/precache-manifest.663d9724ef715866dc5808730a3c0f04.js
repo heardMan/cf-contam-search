f0ca5d9791de92aa0907023d2e114194aa6fc0df
heardman <markyheard@gmail.com>
2020-03-16 22:47:22 -0700

    updated styling

---
+++ build/precache-manifest.663d9724ef715866dc5808730a3c0f04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a74c0d63efa9eb893d732a2adfd38d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46add2be4e92767679aa",
+    "url": "/static/css/main.f6abffa4.chunk.css"
+  },
+  {
+    "revision": "c7c3fed3e9257fef922f",
+    "url": "/static/js/2.f20a0a59.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.f20a0a59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46add2be4e92767679aa",
+    "url": "/static/js/main.4f61601b.chunk.js"
+  },
+  {
+    "revision": "c27f76a36f8d51c29fd4",
+    "url": "/static/js/runtime-main.7bc453c4.js"
+  }
+]);
